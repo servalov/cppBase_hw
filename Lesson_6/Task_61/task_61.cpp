b064bd83b6d3b81746be731bc485f3a9a8f0b4b1
Sergey Valov <sergey.valov.eng@gmail.com>
2025-04-11 19:14:57 +0300

    Добавлены домашние задания к занятию №6.

---
+++ Lesson_6/Task_61/task_61.cpp
@@ -0,0 +1,39 @@
+﻿// Препроцессор и макросы. Задание 1. Препроцессорные директивы
+#include <iostream>
+#include <windows.h>
+
+#define MODE 1
+
+// Проверка определена ли константа MODE
+#ifndef MODE
+#error Необходимо определить MODE !!!
+#endif
+
+double add(double a, double b);   // Функция для сложения двух чисел
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	
+	#if MODE==0
+		std::cout << "Работаю в режиме тренировки" << std::endl;
+	#elif MODE==1
+		std::cout << "Работаю в боевом режиме" << std::endl;
+		double a{}, b{};
+		std::cout << "Введите число 1: ";
+		std::cin >> a;
+		std::cout << "Введите число 2: ";
+		std::cin >> b;
+		std::cout << "Результат сложения: " << add(a, b) << std::endl;
+	#else
+		std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+	#endif
+	
+	return EXIT_SUCCESS;
+}
+
+// Функция для сложения двух чисел
+double add(double a, double b)
+{
+	return a + b;
+}
---
+++ Lesson_6/Task_62/task_62.cpp
@@ -0,0 +1,16 @@
+﻿// Препроцессор и макросы. Задание 2. Макрос SUB
+#include <iostream>
+
+#define SUB(num1,num2) ((num1)-(num2))
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+	return EXIT_SUCCESS;
+}
